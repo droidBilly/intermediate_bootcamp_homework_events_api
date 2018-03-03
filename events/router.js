1a167fc5d18340b73d8c6a1c8b0658a61dc4cc80
Peter Schrammen <b.schrammen@gmail.com>
2018-03-03 14:34:05 +0100

    Adding events possible

--- events/router.js
+++ events/router.js
@@ -3,7 +3,6 @@ const Events = require('./model')
 const router = new Router()
 
 router.get('/events', (request, response) => {
-  console.log(Events)
   Events.findAll()
     .then(result => {
       response.send( result )
@@ -12,6 +11,26 @@ router.get('/events', (request, response) => {
       response.code(404)
       response.send( { message: 'Sorry, it seems to be boring in A\'dam!' })
     })
+})
+
+router.get('/events/:id', (request, response) => {
+  const eventId = request.params.id
+  Events.findById(eventId).then(event => {
+    if (event) {
+      response.send(event)
+    }
+    else {
+      response.status(404)
+      response.json ({ message: "Sorry, no event with this id!"})
+    }
+  })
+})
+
+router.post('/events', (req, res) => {
+  const event = req.body
+  Events.create(event).then(entity => {
+    res.status(201).send(entity)
+  })
 })
 
 module.exports = router
--- index.js
+++ index.js
@@ -13,4 +13,5 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE')
   next()
 })
+
 app.use(eventsRouter)
