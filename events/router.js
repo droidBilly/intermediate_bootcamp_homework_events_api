29425e2d08f3b72519fad7b8e4ae7e47209e4ec1
Peter Schrammen <b.schrammen@gmail.com>
2018-03-03 18:53:57 +0100

    Only futurevents will be shown from /events

--- events/router.js
+++ events/router.js
@@ -2,8 +2,19 @@ const Router = require('express').Router
 const Events = require('./model')
 const router = new Router()
 
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op;
+const today = new Date().toISOString().slice(0,10);
+
 router.get('/events', (request, response) => {
-  Events.findAll()
+  Events.findAll({
+    attributes: ['title', 'start_date', 'end_date' ],
+    where: {
+      start_date : {
+        [Op.gte]: today
+      }
+    }
+  })
     .then(result => {
       response.send( result )
     })
@@ -26,10 +37,36 @@ router.get('/events/:id', (request, response) => {
   })
 })
 
-router.post('/events', (req, res) => {
-  const event = req.body
-  Events.create(event).then(entity => {
-    res.status(201).send(entity)
+router.post('/events', (request, response) => {
+  const event = request.body
+  Events.create(event)
+  .then(entity => {
+    response.status(201).send(entity)
+  })
+  .catch(error => {
+    response.status(500).send({
+      message: `Something went wrong`,
+      error
+    })
+  })
+})
+
+router.delete('/events/:id', (request, response) => {
+  const eventId = Number(request.params.id)
+  Events.findById(request.params.id)
+  .then(entity => {
+    return entity.destroy()
+  })
+  .then(_ => {
+    response.send({
+      message: 'The event was deleted succesfully'
+    })
+  })
+  .catch(error => {
+    response.status(500).send({
+      message: `Something went wrong`,
+      error
+    })
   })
 })
 
