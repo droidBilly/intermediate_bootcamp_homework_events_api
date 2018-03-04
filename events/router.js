8a7e971f781589daf42a7f41723723878ac28ee5
Peter Schrammen <b.schrammen@gmail.com>
2018-03-04 14:49:43 +0100

    Update events possible console.log removed

--- events/router.js
+++ events/router.js
@@ -86,7 +86,6 @@ router.delete('/events/:id', (request, response) => {
 router.put('/events/:id', (request, response) => {
   const eventId = Number(request.params.id)
   const updates = request.body
-  console.log(updates)
   Events.findById(request.params.id)
   .then(entity => {
     return entity.update(updates)
